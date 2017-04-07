bdb36981d9be66d294b6ef319f3b683af4b90374
Luca Di Michele <luca.dimichele@fleetmatics.com>
2017-04-07 16:56:41 +0100

    Initial commit

---
+++ play/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  play
+//
+//  Created by Luca DiMichele on 07/04/2017.
+//  Copyright © 2017 Fleetmatics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
